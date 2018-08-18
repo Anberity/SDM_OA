75d00b2346fc7b6baa762a5830d94b8762483d9b
Anberity <84228473@qq.com>
2018-08-18 23:59:51 +0800

    数据库内容更新

--- WebSite3/WebSite3/App_Code/sqlTable.cs
+++ WebSite3/WebSite3/App_Code/sqlTable.cs
@@ -25,7 +25,7 @@ public class sqlTable
         try
         {
             if (columns.Length == 0 || columns.Length != values.Length) return 0;
-            
+
             sql += table_name;
             sql += "(";
 
@@ -85,7 +85,7 @@ public class sqlTable
                     if (ds.Tables[0].Rows[i][j].ToString() != null)
                     {
                         temp13 = ds.Tables[0].Rows[i][j].ToString();
-                        
+
 
                     }
                     mySql[i][j] = temp13;
@@ -101,7 +101,43 @@ public class sqlTable
         {
         }
     }
-    
+
+    /// <summary>
+    /// 数据库内容修改
+    /// </summary>
+    /// <param name="table_name">表名</param>
+    /// <param name="username">用户名</param>
+    /// <param name="year">年份</param>
+    /// <param name="month">月份</param>
+    /// <param name="columns">列名</param>
+    /// <param name="values">修改值</param>
+    /// <returns>是否成功，1--成功，0--失败</returns>
+    public int table_insert(string table_name, string username, string year, string month, string[] columns, string[] values)
+    {
+        string sql = "UPDATE " + table_name + " SET ";
+        try
+        {
+            if (columns.Length == 0 || columns.Length != values.Length) return 0;
+            int[] length = { columns.Length, values.Length };
+            int a = length.Min();
+
+            for (int i = 0; i < a; i++)
+            {
+                sql += columns[i] + " = '" + values[i] + "',";
+            }
+
+            sql = sql.Substring(0, sql.Length - 1);
+            sql += " WHERE year = " + year + " AND " + "month = " + month + " AND " + "username = " + username;
+
+            int Exe = dal.ExecDataBySql(sql);
+            return 1;
+        }
+        catch (Exception e)
+        {
+            return 0;
+        }
+    }
+
     /// <summary>
     /// 登录查询
     /// </summary>
@@ -155,7 +191,7 @@ public class sqlTable
     /// <returns>是否成功，1成功，0失败</returns>
     public int update_login(string table_name, string username, string pwd)
     {
-        string sql = "UPDATE " + table_name + "SET password = " + pwd + "WHERE username = " + username;
+        string sql = "UPDATE " + table_name + " SET password = " + pwd + " WHERE username = " + username;
         try
         {
             int Exe = dal.ExecDataBySql(sql);
