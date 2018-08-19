cd4d8a106f5764851d763b884341dc2614ac8501
Anberity <84228473@qq.com>
2018-08-19 14:53:30 +0800

    数据库增删改查优化

--- WebSite3/WebSite3/App_Code/sqlTable.cs
+++ WebSite3/WebSite3/App_Code/sqlTable.cs
@@ -13,12 +13,12 @@ public class sqlTable
     DataLogic dal = new DataLogic();
 
     /// <summary>
-    /// 内容添加
+    /// 数据库内容添加
     /// </summary>
     /// <param name="table_name">表名</param>
     /// <param name="columns">列名</param>
     /// <param name="values">插入内容</param>
-    /// <returns>是否成功</returns>
+    /// <returns>是否成功，0--失败，1--成功</returns>
     public int table_insert(string table_name, string[] columns, string[] values)
     {
         string sql = "insert into ";
@@ -45,6 +45,74 @@ public class sqlTable
             sql = sql.Substring(0, sql.Length - 3);
             sql += "')";
 
+
+            int Exe = dal.ExecDataBySql(sql);
+            return 1;
+        }
+        catch (Exception e)
+        {
+            return 0;
+        }
+    }
+
+    /// <summary>
+    /// 数据库内容删除
+    /// </summary>
+    /// <param name="table_name">表名</param>
+    /// <param name="columns">列名</param>
+    /// <param name="values">列值</param>
+    /// <returns>是否成功，0--失败，1--成功</returns>
+    public int table_delete(string table_name, string[] columns, string[] values)
+    {
+        string sql = "DELETE FROM " + table_name + "WHERE ";
+        try
+        {
+            if (columns.Length == 0 || columns.Length != values.Length) return 0;
+
+            int[] length = { columns.Length, values.Length };
+            int a = length.Min();
+
+            for (int i = 0; i < a; i++)
+            {
+                sql += columns[i] + " = " + values[i] + " AND ";
+            }
+            sql = sql.Substring(0, sql.Length - 5);
+
+            int Exe = dal.ExecDataBySql(sql);
+            return 1;
+        }
+        catch (Exception e)
+        {
+            return 0;
+        }
+    }
+
+    /// <summary>
+    /// 数据库内容修改
+    /// </summary>
+    /// <param name="table_name">表名</param>
+    /// <param name="username">用户名</param>
+    /// <param name="year">年份</param>
+    /// <param name="month">月份</param>
+    /// <param name="columns">列名</param>
+    /// <param name="values">修改值</param>
+    /// <returns>是否成功，1--成功，0--失败</returns>
+    public int table_update(string table_name, string username, string year, string month, string[] columns, string[] values)
+    {
+        string sql = "UPDATE " + table_name + " SET ";
+        try
+        {
+            if (columns.Length == 0 || columns.Length != values.Length) return 0;
+            int[] length = { columns.Length, values.Length };
+            int a = length.Min();
+
+            for (int i = 0; i < a; i++)
+            {
+                sql += columns[i] + " = '" + values[i] + "',";
+            }
+
+            sql = sql.Substring(0, sql.Length - 1);
+            sql += " WHERE year = " + year + " AND " + "month = " + month + " AND " + "username = " + username;
 
             int Exe = dal.ExecDataBySql(sql);
             return 1;
@@ -101,67 +169,7 @@ public class sqlTable
         {
         }
     }
-
-    /// <summary>
-    /// 数据库内容修改
-    /// </summary>
-    /// <param name="table_name">表名</param>
-    /// <param name="username">用户名</param>
-    /// <param name="year">年份</param>
-    /// <param name="month">月份</param>
-    /// <param name="columns">列名</param>
-    /// <param name="values">修改值</param>
-    /// <returns>是否成功，1--成功，0--失败</returns>
-    public int table_insert(string table_name, string username, string year, string month, string[] columns, string[] values)
-    {
-        string sql = "UPDATE " + table_name + " SET ";
-        try
-        {
-            if (columns.Length == 0 || columns.Length != values.Length) return 0;
-            int[] length = { columns.Length, values.Length };
-            int a = length.Min();
-
-            for (int i = 0; i < a; i++)
-            {
-                sql += columns[i] + " = '" + values[i] + "',";
-            }
-
-            sql = sql.Substring(0, sql.Length - 1);
-            sql += " WHERE year = " + year + " AND " + "month = " + month + " AND " + "username = " + username;
-
-            int Exe = dal.ExecDataBySql(sql);
-            return 1;
-        }
-        catch (Exception e)
-        {
-            return 0;
-        }
-    }
-
-    /// <summary>
-    /// 数据库定点删除
-    /// </summary>
-    /// <param name="table_name">表名</param>
-    /// <param name="username">用户名</param>
-    /// <param name="year">年份</param>
-    /// <param name="month">月份</param>
-    /// <returns>是否成功，1--成功，0--失败</returns>
-    public int table_delete(string table_name, string username, string year, string month)
-    {
-        string sql = "DELETE FROM " + table_name;
-        try
-        {
-            sql += " WHERE year = " + year + " AND " + "month = " + month + " AND " + "username = " + username;
-
-            int Exe = dal.ExecDataBySql(sql);
-            return 1;
-        }
-        catch (Exception e)
-        {
-            return 0;
-        }
-    }
-
+   
     /// <summary>
     /// 登录查询
     /// </summary>
@@ -213,33 +221,11 @@ public class sqlTable
     /// <param name="username">用户名</param>
     /// <param name="pwd">新密码</param>
     /// <returns>是否成功，1--成功，0--失败</returns>
-    public int update_login(string table_name, string username, string pwd)
+    public int update_login(string table_name, string name, string pwd)
     {
-        string sql = "UPDATE " + table_name + " SET password = " + pwd + " WHERE username = " + username;
+        string sql = "UPDATE " + table_name + " SET password = " + pwd + " WHERE name = " + name;
         try
         {
-            int Exe = dal.ExecDataBySql(sql);
-            return 1;
-        }
-        catch (Exception e)
-        {
-            return 0;
-        }
-    }
-
-    /// <summary>
-    /// 删除用户
-    /// </summary>
-    /// <param name="table_name">表名</param>
-    /// <param name="name">员工姓名</param>
-    /// <returns>是否成功，1--成功，0--失败</returns>
-    public int delete_login(string table_name, string name)
-    {
-        string sql = "DELETE FROM " + table_name;
-        try
-        {
-            sql += " WHERE name = " + name;
-
             int Exe = dal.ExecDataBySql(sql);
             return 1;
         }
