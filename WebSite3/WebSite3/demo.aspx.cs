213f612901f34481f8ad849f5969a3fbc8a395ea
Anberity <84228473@qq.com>
2018-08-23 09:11:06 +0800

    test

--- WebSite3/WebSite3/demo.aspx.cs
+++ WebSite3/WebSite3/demo.aspx.cs
@@ -10,14 +10,14 @@ public partial class demo : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        string constr = "data source=DESKTOP-JFMFAQ0;initial catalog=OA;user id=sa;pwd=67712563";
-        string sql = "SELECT * FROM Debug WHERE year=" + DateTime.Now.Year.ToString() + " AND month=" + DateTime.Now.Year.ToString();//+  " AND username=zdhhyz";
+        /*string constr = "data source=DESKTOP-JFMFAQ0;initial catalog=OA;user id=sa;pwd=67712563";
+        string sql = "SELECT * FROM Debug WHERE year=" + DateTime.Now.Year.ToString() + " AND month=" + DateTime.Now.Month.ToString();//+  " AND username=zdhhyz";
         //projectname,site,manageday,debugday,remark
         SqlConnection conn = new SqlConnection(constr);
         conn.Open();
         SqlCommand cmd = new SqlCommand(sql, conn);
 
         Repeater1.DataSource = cmd.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
-        Repeater1.DataBind();
+        Repeater1.DataBind();*/
     }
 }
