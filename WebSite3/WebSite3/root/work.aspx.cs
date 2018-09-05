b813f37f85508472fbba30f7443ec53d3098aaa2
Anberity <84228473@qq.com>
2018-09-05 14:07:58 +0800

    绝大部分注销添加

--- WebSite3/WebSite3/root/work.aspx.cs
+++ WebSite3/WebSite3/root/work.aspx.cs
@@ -9,23 +9,37 @@ public partial class root_work : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        string name = "胡芸志";
-        Response.Write(" <script>window.onload=function(){ var name=document.getElementById('name'); name.innerHTML='欢迎你，" + name + "'} </script> ");
         try
         {
             if (HttpContext.Current.Session["username"].ToString() == "null" || HttpContext.Current.Session["userpwd"].ToString() == "null")
             {
-                HttpContext.Current.Response.Write(" <script> alert( '您还未登陆，请先登录！！！');window.location.href= 'Default.aspx ' </script> ");
+                HttpContext.Current.Response.Write(" <script> alert( '您还未登陆，请先登录！！！');window.location.href= '../Default.aspx ' </script> ");
             }
         }
         catch (Exception)
         {
-            HttpContext.Current.Response.Write(" <script> alert( '您还未登陆，请先登录！！！');window.location.href= 'Default.aspx ' </script> ");
+            HttpContext.Current.Response.Write(" <script> alert( '您还未登陆，请先登录！！！');window.location.href= '../Default.aspx ' </script> ");
         }
+
+        string name = HttpContext.Current.Session["name"].ToString();
+        Response.Write(" <script>window.onload=function(){ var name=document.getElementById('name'); name.innerHTML='欢迎你，" + name + "'} </script> ");
     }
 
     protected void logout_Click(object sender, EventArgs e)
     {
-
+        HttpContext.Current.Session["power"] = "null";//权限
+        HttpContext.Current.Session["username"] = "null";//获取用户名
+        HttpContext.Current.Session["userpwd"] = "null";//获取密码
+        HttpContext.Current.Session["name"] = "null";//获取用户名字
+        HttpContext.Current.Session["team"] = "null";//获取用户小组
+        HttpContext.Current.Session["yearh"] = DateTime.Now.Year.ToString();//历史年份
+        HttpContext.Current.Session["monh"] = DateTime.Now.Month.ToString();//历史月份
+        HttpContext.Current.Session["months"] = DateTime.Now.Month.ToString();//汇总查看月份
+        HttpContext.Current.Session["years"] = DateTime.Now.Month.ToString();//汇总查看年份
+        HttpContext.Current.Session["yearuser"] = DateTime.Now.Month.ToString();//按年查看员工汇总
+        HttpContext.Current.Session["numberMonth"] = "100";//月份汇总
+        HttpContext.Current.Session["numberYear"] = "100";//年份汇总
+        HttpContext.Current.Session["userYear"] = "100";//员工年份汇总
+        HttpContext.Current.Response.Write(" <script>window.location.href= '../Default.aspx' </script> ");
     }
 }
